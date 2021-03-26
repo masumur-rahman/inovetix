f024ed1a4ff8c6e3cf599d7abb9a07e210684e38
Masumur Rahman <csemasum@gmail.com>
2021-03-26 21:54:46 +0600

    new inovetix ui added 26

--- js/main.js
+++ js/main.js
@@ -139,7 +139,7 @@ $.scrollUp({
 	animation: 'fade', // Fade, slide, none
 	animationInSpeed: 200, // Animation in speed (ms)
 	animationOutSpeed: 200, // Animation out speed (ms)
-	scrollText: '<i class="icofont icofont-long-arrow-up"></i>', // Text for element
+	scrollText: '<i class="fa fa-arrow-up"></i>', // Text for element
 	activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
 });
 
